b8a08e52f2065739a25f14cec72293dd6c079740
tianxiuyi <tianxiuyi@126.com>
2025-06-23 15:42:04 +0800

    新增itemHead

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 import Layout from '@/layout'
+import ItemHead from '@/components/itemHead'
+
 
 const typeName = {
   submit: '提交',
@@ -11,344 +13,356 @@ const typeName = {
 }
 
 export const routes = [
-	{
-		path: '/',
-		name: 'Layout',
-		component: Layout,
-		meta: {base_path: '/'},
-		children: [
-			{
-				path: 'MyToDoList',
-				name: 'MyToDoList',
-				meta: {title: '我的待办'},
-				component: () => import('@/view/MyToDo/MyToDoList.vue')
-			},
-			{
-				path: '/PlannedManagementList',
-				name: 'PlannedManagementList',
-				meta: {title: '计划管理'},
-				component: () => import('@/view/PlannedManagement/PlannedManagementList.vue')
-			},
-			{
-				path: '/AcceptanceReturn',
-				name: 'AcceptanceReturn',
-				meta: {title: '验收管理'},
-				component: () => import('@/view/AcceptanceReturn/AcceptanceReturn.vue')
-			},
-			{
-				path: '/InOutManagementList',
-				name: 'InOutManagementList',
-				meta: {title: '出入库管理'},
-				component: () => import('@/view/InOutManagement/InOutManagementList.vue')
-			},
-			{
-				path: '/WaitExamineList',
-				name: 'WaitExamineList',
-				meta: {title: '待审核'},
-				component: () => import('@/view/SupervisionUnit/WaitExamineList.vue')
-			},
-			{
-				path: '/WaitHandleList',
-				name: 'WaitHandleList',
-				meta: {title: '待处理'},
-				component: () => import('@/view/SupervisionUnit/WaitHandleList.vue')
-			},
-			{
-				path: '/OverExamineList',
-				name: 'OverExamineList',
-				meta: {title: '已审核'},
-				component: () => import('@/view/SupervisionUnit/OverExamineList.vue')
-			},
-			{
-				path: '/dashboard',
-				name: 'dashboard',
-				meta: {title: '需求管理'},
-				component: () => import('@/view/Demand/index.vue')
-			},
-			{
-				path: '/Information',
-				name: 'Information',
-				meta: {title: '发货管理'},
-				component: () => import('@/view/Demand/Delivery.vue')
-			},
-			{
-				path: '/MyManager',
-				name: 'MyManager',
-				meta: {title: '退货管理'},
-				component: () => import('@/view/Demand/Return.vue')
-			},
-		]
-	},
-	{
-		path: '/MyToDoManager',
-		component: () => import('@/view/MyToDo/MyToDoManager/index.vue'),
-		redirect: '/MyToDoManager/MyToDoDetail',
-		hidden: true,
-		children: [
-			{
-				path: 'MyToDoDetail/:type',
-				name: 'MyToDoDetail',
-				meta: {title: '我的待办-详情'},
-				component: () => import('@/view/MyToDo/MyToDoManager/MyToDoDetail.vue')
-			},
-			{
-				path: 'MyToDoExamineEdit/:type',
-                name: 'MyToDoExamineEdit',
-				meta: {title: '审核意见'},
-				component: () => import('@/view/MyToDo/MyToDoManager/MyToDoExamineEdit.vue')
-			},
-			{
-				path: 'MyToDoExamineChoice',
-                name: 'MyToDoExamineChoice',
-				meta: {title: '选择审批人'},
-				component: () => import('@/view/MyToDo/MyToDoManager/MyToDoExamineChoice.vue')
-			},
-			{
-				path: 'MyToDoSearch',
-                name: 'MyToDoSearch',
-				meta: {title: '搜索'},
-				component: () => import('@/view/MyToDo/MyToDoManager/MyToDoSearch.vue')
-			},
-		]
-	},
-	{
-		path: '/SupervisionUnitManager',
-		component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/index.vue'),
-		redirect: '/SupervisionUnitManager/SupervisionUnitDetail',
-		hidden: true,
-		children: [
-			{
-				path: '/SupervisionUnitDetail/:type',
-				name: 'SupervisionUnitDetail',
-				meta: {title: '单据详情'},
-				component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitDetail.vue')
-			},
-			{
-				path: '/SupervisionUnitExamineEdit/:type',
-                name: 'SupervisionUnitExamineEdit',
-				meta: {title: '审核意见'},
-				component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitExamineEdit.vue')
-			},
-			{
-				path: '/SupervisionUnitExamineChoice',
-                name: 'SupervisionUnitExamineChoice',
-				meta: {title: '选择审批人'},
-				component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitExamineChoice.vue')
-			},
-		]
-	},
-	{
-		path: '/MyProcess',
-		name: 'MyProcess',
-		meta: {title: '查看流程'},
-		component: () => import('@/view/MyProcess/index.vue')
-	},
   {
-		path: '/PlannedManagementChild',
-    component: () => import('@/view/PlannedManagement/PlannedManagementChild/index.vue'),
-		hidden: true,
-		children: [
+    path: '/',
+    name: 'Layout',
+    component: Layout,
+    meta: { base_path: '/' },
+    children: [
+      {
+        path: 'MyToDoList',
+        name: 'MyToDoList',
+        meta: { title: '我的待办' },
+        component: () => import('@/view/MyToDo/MyToDoList.vue')
+      },
+      {
+        path: '/PlannedManagementList',
+        name: 'PlannedManagementList',
+        meta: { title: '计划管理' },
+        component: () => import('@/view/PlannedManagement/PlannedManagementList.vue')
+      },
+      {
+        path: '/AcceptanceReturn',
+        name: 'AcceptanceReturn',
+        meta: { title: '验收管理' },
+        component: () => import('@/view/AcceptanceReturn/AcceptanceReturn.vue')
+      },
+      {
+        path: '/InOutManagementList',
+        name: 'InOutManagementList',
+        meta: { title: '出入库管理' },
+        component: () => import('@/view/InOutManagement/InOutManagementList.vue')
+      },
+      {
+        path: '/WaitExamineList',
+        name: 'WaitExamineList',
+        meta: { title: '待审核' },
+        component: () => import('@/view/SupervisionUnit/WaitExamineList.vue')
+      },
+      {
+        path: '/WaitHandleList',
+        name: 'WaitHandleList',
+        meta: { title: '待处理' },
+        component: () => import('@/view/SupervisionUnit/WaitHandleList.vue')
+      },
+      {
+        path: '/OverExamineList',
+        name: 'OverExamineList',
+        meta: { title: '已审核' },
+        component: () => import('@/view/SupervisionUnit/OverExamineList.vue')
+      },
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        meta: { title: '需求管理' },
+        component: () => import('@/view/Demand/index.vue')
+      },
+      {
+        path: '/Information',
+        name: 'Information',
+        meta: { title: '发货管理' },
+        component: () => import('@/view/Demand/Delivery.vue')
+      },
+      {
+        path: '/MyManager',
+        name: 'MyManager',
+        meta: { title: '退货管理' },
+        component: () => import('@/view/Demand/Return.vue')
+      },
+    ]
+  },
+  {
+    path: '/MyToDoManager',
+    component: ItemHead,
+    hidden: true,
+    children: [
+      {
+        path: 'MyToDoDetail/:type',
+        name: 'MyToDoDetail',
+        meta: { title: '我的待办-详情' },
+        component: () => import('@/view/MyToDo/MyToDoManager/MyToDoDetail.vue')
+      },
+      {
+        path: 'MyToDoExamineEdit/:type',
+        name: 'MyToDoExamineEdit',
+        meta: { title: '审核意见' },
+        component: () => import('@/view/MyToDo/MyToDoManager/MyToDoExamineEdit.vue')
+      },
+      {
+        path: 'MyToDoExamineChoice',
+        name: 'MyToDoExamineChoice',
+        meta: { title: '选择审批人' },
+        component: () => import('@/view/MyToDo/MyToDoManager/MyToDoExamineChoice.vue')
+      },
+      {
+        path: 'MyToDoSearch',
+        name: 'MyToDoSearch',
+        meta: { title: '搜索' },
+        component: () => import('@/view/MyToDo/MyToDoManager/MyToDoSearch.vue')
+      },
+    ]
+  },
+  {
+    path: '/SupervisionUnitManager',
+    component: ItemHead,
+    hidden: true,
+    children: [
+      {
+        path: '/SupervisionUnitDetail/:type',
+        name: 'SupervisionUnitDetail',
+        meta: { title: '单据详情' },
+        component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitDetail.vue')
+      },
+      {
+        path: '/SupervisionUnitExamineEdit/:type',
+        name: 'SupervisionUnitExamineEdit',
+        meta: { title: '审核意见' },
+        component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitExamineEdit.vue')
+      },
+      {
+        path: '/SupervisionUnitExamineChoice',
+        name: 'SupervisionUnitExamineChoice',
+        meta: { title: '选择审批人' },
+        component: () => import('@/view/SupervisionUnit/SupervisionUnitManager/SupervisionUnitExamineChoice.vue')
+      },
+    ]
+  },
+  {
+    path: '/MyProcess',
+    component: ItemHead,
+    hidden: true,
+    children: [
+      {
+        path: '/MyProcess',
+        name: 'MyProcess',
+        meta: { title: '查看流程' },
+        component: () => import('@/view/MyProcess/index.vue')
+      },
+    ]
+  },
+  {
+    path: '/PlannedManagementChild',
+    component: ItemHead,
+    hidden: true,
+    children: [
       {
         path: 'RequirementFilling',
         name: 'RequirementFilling',
-        meta: {title: '需求填报'},
+        meta: { title: '需求填报' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/RequirementFilling.vue')
       },
       {
         path: 'SelectContract',
         name: 'SelectContract',
-        meta: {title: '选择合同'},
+        meta: { title: '选择合同' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/SelectContract.vue')
       },
       {
         path: 'SelectMaterials',
         name: 'SelectMaterials',
-        meta: {title: '选择物资'},
+        meta: { title: '选择物资' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/SelectMaterials.vue')
       },
       {
         path: 'SaveMaterials',
         name: 'SaveMaterials',
-        meta: {title: '保存物资'},
+        meta: { title: '保存物资' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/SaveMaterials.vue')
       },
       {
         path: 'RequirementDetails',
         name: 'RequirementDetails',
-        meta: {title: '需求详情'},
+        meta: { title: '需求详情' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/RequirementDetails.vue')
       },
       {
         path: 'SupplyOverview',
         name: 'SupplyOverview',
-        meta: {title: '供应概览'},
+        meta: { title: '供应概览' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/SupplyOverview.vue')
       },
       {
         path: 'LogisticsView',
         name: 'LogisticsView',
-        meta: {title: '查看物流'},
+        meta: { title: '查看物流' },
         component: () => import('@/view/PlannedManagement/PlannedManagementChild/LogisticsView.vue')
       },
-		]
-	},
+    ]
+  },
   {
-		path: '/InOutManagementChild',
-    component: () => import('@/view/InOutManagement/InOutManagementChild/index.vue'),
-		hidden: true,
-		children: [
+    path: '/InOutManagementChild',
+    component: ItemHead,
+    hidden: true,
+    children: [
       {
         path: 'SubmitStore',
         name: 'SubmitStore',
-        meta: {title: '提交入库'},
+        meta: { title: '提交入库' },
         component: () => import('@/view/InOutManagement/InOutManagementChild/SubmitStore.vue'),
         beforeEnter(to, from, next) {
-            to.meta.title = to.query.type == 'view' ? `入库${typeName[to.query.type]}` : `${typeName[to.query.type]}入库`
-            next()
+          to.meta.title = to.query.type == 'view' ? `入库${typeName[to.query.type]}` : `${typeName[to.query.type]}入库`
+          next()
         }
       },
       {
         path: 'Outbound',
         name: 'Outbound',
-        meta: {title: '物资出库'},
+        meta: { title: '物资出库' },
         component: () => import('@/view/InOutManagement/InOutManagementChild/Outbound.vue'),
         beforeEnter(to, from, next) {
-            to.meta.title = to.query.type == 'view' ? `出库${typeName[to.query.type]}` : `物资出库`
-            next()
+          to.meta.title = to.query.type == 'view' ? `出库${typeName[to.query.type]}` : `物资出库`
+          next()
         }
       },
       {
         path: 'OutboundDetails',
         name: 'OutboundDetails',
-        meta: {title: '出库详情'},
+        meta: { title: '出库详情' },
         component: () => import('@/view/InOutManagement/InOutManagementChild/OutboundDetails.vue'),
       },
       {
         path: 'InventoryDetails',
         name: 'InventoryDetails',
-        meta: {title: '库存详情'},
+        meta: { title: '库存详情' },
         component: () => import('@/view/InOutManagement/InOutManagementChild/InventoryDetails.vue'),
       }
     ]
   },
-	{
-		path: '/AcceptanceReturnChild',
-		component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/index.vue'),
-		hidden: true,
-		children: [
-			{
-				path: 'DoAccept',
-				name: 'DoAccept',
-				meta: {title: '初验收货'},
-				component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoAccept.vue')
-			},
-			{
-				path: 'DoAcceptDetail',
-				name: 'DoAcceptDetail',
-				meta: {title: '收货详情'},
-				component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoAcceptDetail.vue')
-			},
-			{
-				path: 'DoReturn',
-				name: 'DoReturn',
-				meta: {title: '退货详情'},
-				component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoReturn.vue')
-			},
-		]
-	},
-	// 确认需求
-	{
-		
-		path: '/confirm',
-		component: () => import('@/view/Demand/Confirm/index.vue'),
-		hidden: true,
-		name: 'confirm',
-		meta: {title: '确认需求'},
-	},
-	{
-		
-		path: '/sendGoods',
-		component: () => import('@/view/Demand/SendGoods/index.vue'),
-		hidden: true,
-		name: 'SendGoods',
-		meta: {title: '发货'},
-	},
-	{
-		
-		path: '/selectGoods',
-		component: () => import('@/view/Demand/SendGoods/selectGoods.vue'),
-		hidden: true,
-		name: 'selectGoods',
-		meta: {title: '选择物资'},
-	},
-	{
-		
-		path: '/finishGoods',
-		component: () => import('@/view/Demand/SendGoods/finishGoods.vue'),
-		hidden: true,
-		name: 'finishGoods',
-		meta: {title: '选择完物资'},
-	},
-	{
-		
-		path: '/supplyMsg',
-		component: () => import('@/view/Demand/Supply/index.vue'),
-		hidden: true,
-		name: 'supplyMsg',
-		meta: {title: '供应信息'},
-	},
-	{
-		
-		path: '/lookUp',
-		component: () => import('@/view/Demand/LookUp/index.vue'),
-		hidden: true,
-		name: 'lookUp',
-		meta: {title: '查看物流'},
-	},
-	{
-		
-		path: '/lookCargo',
-		component: () => import('@/view/Demand/Cargo/index.vue'),
-		hidden: true,
-		name: 'lookCargo',
-		meta: {title: '货运详情'},
-	},
-	{
-		
-		path: '/cargoDetails',
-		component: () => import('@/view/Demand/Cargo/cargoDetails.vue'),
-		hidden: true,
-		name: 'cargoDetails',
-		meta: {title: '货运详情'},
-	},
-	{
-		
-		path: '/returnDetails',
-		component: () => import('@/view/Demand/ReturnGoods/index.vue'),
-		hidden: true,
-		name: 'returnDetails',
-		meta: {title: '退货详情'},
-	},
+  {
+    path: '/AcceptanceReturnChild',
+    component: ItemHead,
+    hidden: true,
+    children: [
+      {
+        path: 'DoAccept',
+        name: 'DoAccept',
+        meta: { title: '初验收货' },
+        component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoAccept.vue')
+      },
+      {
+        path: 'DoAcceptDetail',
+        name: 'DoAcceptDetail',
+        meta: { title: '收货详情' },
+        component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoAcceptDetail.vue')
+      },
+      {
+        path: 'DoReturn',
+        name: 'DoReturn',
+        meta: { title: '退货详情' },
+        component: () => import('@/view/AcceptanceReturn/AcceptanceReturnChild/DoReturn.vue')
+      },
+    ]
+  },
+  // 确认需求
+  {
+    path: '/Demand',
+    component: ItemHead,
+    hidden: true,
+    children: [
+      {
+
+        path: '/confirm',
+        component: () => import('@/view/Demand/Confirm/index.vue'),
+        hidden: true,
+        name: 'confirm',
+        meta: { title: '确认需求' },
+      },
+      {
+
+        path: '/sendGoods',
+        component: () => import('@/view/Demand/SendGoods/index.vue'),
+        hidden: true,
+        name: 'SendGoods',
+        meta: { title: '发货' },
+      },
+      {
+
+        path: '/selectGoods',
+        component: () => import('@/view/Demand/SendGoods/selectGoods.vue'),
+        hidden: true,
+        name: 'selectGoods',
+        meta: { title: '选择物资' },
+      },
+      {
+
+        path: '/finishGoods',
+        component: () => import('@/view/Demand/SendGoods/finishGoods.vue'),
+        hidden: true,
+        name: 'finishGoods',
+        meta: { title: '选择完物资' },
+      },
+      {
+
+        path: '/supplyMsg',
+        component: () => import('@/view/Demand/Supply/index.vue'),
+        hidden: true,
+        name: 'supplyMsg',
+        meta: { title: '供应信息' },
+      },
+      {
+
+        path: '/lookUp',
+        component: () => import('@/view/Demand/LookUp/index.vue'),
+        hidden: true,
+        name: 'lookUp',
+        meta: { title: '查看物流' },
+      },
+      {
+
+        path: '/lookCargo',
+        component: () => import('@/view/Demand/Cargo/index.vue'),
+        hidden: true,
+        name: 'lookCargo',
+        meta: { title: '货运详情' },
+      },
+      {
+
+        path: '/cargoDetails',
+        component: () => import('@/view/Demand/Cargo/cargoDetails.vue'),
+        hidden: true,
+        name: 'cargoDetails',
+        meta: { title: '货运详情' },
+      },
+      {
+
+        path: '/returnDetails',
+        component: () => import('@/view/Demand/ReturnGoods/index.vue'),
+        hidden: true,
+        name: 'returnDetails',
+        meta: { title: '退货详情' },
+      },
+    ]
+  }
 ];
 
 // add route path
 routes.forEach(route => {
-	route.path = route.path || '/' + (route.title || '');
+  route.path = route.path || '/' + (route.title || '');
 });
 
 const router = new Router({
-	mode: 'history',
-	routes,
-	// fallback: true,
-	scrollBehavior: function (to, from, savePostion) {
-		return savePostion ? savePostion:{y:0}
-	}
+  mode: 'history',
+  routes,
+  // fallback: true,
+  scrollBehavior: function (to, from, savePostion) {
+    return savePostion ? savePostion : { y: 0 }
+  }
 
 });
 
 //解决重复导航报错
 const VueRouterPush = Router.prototype.push
-Router.prototype.push = function push (to) {
+Router.prototype.push = function push(to) {
   return VueRouterPush.call(this, to).catch(err => err)
 }
 
