61d761f00aadf9b11b7736834cea6b6927448511
yaoyuan <1537693643@qq.com>
2025-07-15 09:13:38 +0800

    "完善发货"

--- src/view/Demand/SendGoods/finishGoods.vue
+++ src/view/Demand/SendGoods/finishGoods.vue
@@ -1,72 +1,73 @@
 <template>
   <div class="finish">
     <div class="main">
-      <div v-if="goodsData.length>0">
+      <div v-if="goodsData.length > 0">
         <div class="box" v-for="(item, index) in goodsData" :key="index">
-        <div class="detail-base-info">
-          <div class="detail-title-content" style="padding-right: 0px;">
-                <img src="/static/icon-xqjh.png">
-                <span>物资名称：</span>
-                <span>{{ item.materialName }}</span>
-                <div class="detail-title-status">
-                    
-                    <span @click="delgoods(index)" style="color: red;font-size: 0.3rem;">删除</span>
-                </div>
+          <div class="detail-base-info">
+            <div class="detail-title-content" style="padding-right: 0px;">
+              <img src="/static/icon-xqjh.png">
+              <span>物资名称：</span>
+              <span>{{ item.materialName }}</span>
+              <div class="detail-title-status">
+
+                <span @click="delgoods(index)" style="color: red;font-size: 0.3rem;">删除</span>
+              </div>
             </div>
-          <div>
-            <ul class="detail-list-ul">
-              <li>
-                <span>规格型号：</span>
-                <span>{{ item.specModel }}</span>
-              </li>
-              <li>
-                <span>计量单位：</span>
-                <span>{{ item.unit }}</span>
-              </li>
-              <li>
-                <span style="min-width: 2rem;">次计划数量：</span>
-                <span>{{ item.planAmount }}</span>
-              </li>
-              <li style="color: red;">
-                <span style="min-width: 4rem;">本次需求未发货数量：</span>
-                <span>{{ item.ssendTotal }}</span>
-              </li>
-            </ul>
+            <div>
+              <ul class="detail-list-ul">
+                <li>
+                  <span>规格型号：</span>
+                  <span>{{ item.specModel }}</span>
+                </li>
+                <li>
+                  <span>计量单位：</span>
+                  <span>{{ item.unit }}</span>
+                </li>
+                <li>
+                  <span style="min-width: 2rem;">次计划数量：</span>
+                  <span>{{ item.planAmount }}</span>
+                </li>
+                <li style="color: red;">
+                  <span style="min-width: 4rem;">本次需求未发货数量：</span>
+                  <span>{{ item.ssendTotal }}</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <van-divider />
+          <div class="detail-list-ul">
+            <van-form ref="form">
+              <van-field v-model="goodsData[index].sendTotal" required label="发货数量" placeholder="发货数量"
+                input-align="right" />
+              <van-field v-model="goodsData[index].packagingFm" @click.stop="fieldClick($event, 'packagingFm', index)"
+                required label="包装形式" placeholder="请输入包装形式" input-align="right" />
+              <van-field readonly clickable v-model="goodsData[index].manufactureDate" required name="datetimePicker"
+                :value="goodsData[index].manufactureDate" label="生产日期" placeholder="点击选择日期"
+                @click="showCalendars(item, index, 'show')" input-align="right" />
+              <van-field readonly clickable v-model="goodsData[index].expirationDate" name="datetimePicker"
+                :value="goodsData[index].expirationDate" label="有效截止日期" placeholder="有效截止日期"
+                @click="showCalendars(item, index, 'end')" input-align="right" />
+              <van-field v-model="goodsData[index].field2" label="收货地址" required placeholder="收货地址"
+                input-align="right" />
+              <van-field readonly clickable v-model="goodsData[index].supplyDate" name="datetimePicker" required
+                :value="goodsData[index].supplyDate" label="供应时间" placeholder="点击选择时间"
+                @click="showCalendars(item, index, 'gong')" input-align="right" />
+              <van-field v-model="goodsData[index].receiver" name="收货人" required label="收货人和电话" placeholder="收货人"
+                input-align="right" />
+              <van-field v-model="goodsData[index].addr" label="使用地点" placeholder="使用地点" input-align="right" disabled />
+              <van-field v-model="goodsData[index].field0" :name="goodsData[index].field0" label="投资方" required
+                placeholder="投资方" disabled input-align="right" />
+              <van-field v-model="goodsData[index].field1" name="投资比例" required label="投资比例" disabled placeholder="投资比例"
+                input-align="right" />
+
+              <van-field v-model="goodsData[index].remark" label="备注" placeholder="请输入备注" input-align="right" />
+            </van-form>
+          </div>
+          <div style="padding-right: 0.5rem;">
+            <file-upload-view title="合格证附件" :fileList="goodsData[index].fileList01" businessType="01" />
+            <file-upload-view title="厂检报告附件" :fileList="goodsData[index].fileList02" businessType="01" />
           </div>
         </div>
-        <van-divider />
-        <div class="detail-list-ul">
-          <van-form ref="form">
-            <van-field v-model="goodsData[index].sendTotal" required label="发货数量" placeholder="发货数量"
-              input-align="right" />
-            <van-field v-model="goodsData[index].packagingFm" @click.stop="fieldClick($event, 'packagingFm', index)" required label="包装形式" placeholder="请输入包装形式"
-              input-align="right" />
-            <van-field readonly clickable v-model="goodsData[index].manufactureDate" required name="datetimePicker"
-              :value="goodsData[index].manufactureDate" label="生产日期" placeholder="点击选择日期"
-              @click="showCalendars(item, index, 'show')" input-align="right" />
-            <van-field readonly clickable v-model="goodsData[index].expirationDate" name="datetimePicker"
-              :value="goodsData[index].expirationDate" label="有效截止日期" placeholder="有效截止日期"
-              @click="showCalendars(item, index, 'end')" input-align="right" />
-            <van-field v-model="goodsData[index].field2" label="收货地址" required placeholder="收货地址" input-align="right" />
-            <van-field readonly clickable v-model="goodsData[index].supplyDate" name="datetimePicker" required
-              :value="goodsData[index].supplyDate" label="供应时间" placeholder="点击选择时间"
-              @click="showCalendars(item, index, 'gong')" input-align="right" />
-            <van-field v-model="goodsData[index].receiver" name="收货人" required label="收货人和电话" placeholder="收货人"
-              input-align="right" />
-            <van-field v-model="goodsData[index].addr" label="使用地点" placeholder="使用地点" input-align="right" disabled />
-            <van-field v-model="goodsData[index].field0" :name="goodsData[index].field0" label="投资方" required
-              placeholder="投资方" disabled input-align="right" />
-            <van-field v-model="goodsData[index].field1" name="投资比例" required label="投资比例" disabled placeholder="投资比例"
-              input-align="right" />
-
-            <van-field v-model="goodsData[index].remark" label="备注" placeholder="请输入备注" input-align="right" />
-          </van-form>
-        </div>
-        <div style="padding-right: 0.5rem;">
-          <file-upload-view title="合格证附件" :fileList="goodsData[index].fileList01" businessType="01" />
-          <file-upload-view title="厂检报告附件" :fileList="goodsData[index].fileList02" businessType="01" />
-        </div>
-      </div>
       </div>
       <div v-else>
         <van-empty description="暂无数据" />
@@ -138,7 +139,7 @@ export default {
       fileList01: [],
       fileList02: [],
       params: {},
-      planId:""
+      planId: ""
       // fileList1:[],
       // fileList2:[]
     };
@@ -147,39 +148,39 @@ export default {
     this.goodsId = this.$route.query.id
     // 带回来的编辑标识
     this.text = this.$route.query.text,
-    this.planId = this.$route.query.planId
+      this.planId = this.$route.query.planId
     if (this.text == 'add') {
       this.goodsData = _.cloneDeep(JSON.parse(this.$route.query.goodData)).map(item => ({
         ...item,
         planDetailId: item.id,
         fileList01: [],
         fileList02: [],
-        manufactureDate:'',
-        expirationDate:"",
+        manufactureDate: '',
+        expirationDate: "",
         sendTotal: item.ssendTotal
       }))
     }
     if (this.text == 'edit') {
       let editGoodsData = JSON.parse(this.$route.query.goodData)
-      if(Array.isArray(editGoodsData) && editGoodsData.length>0){
-        
+      if (Array.isArray(editGoodsData) && editGoodsData.length > 0) {
+
         this.goodsData = editGoodsData.map(item => ({
-        ...item,
-        planDetailId: item.id,
-        fileList01: [],
-        fileList02: [],
-        manufactureDate:'',
-        expirationDate:"",
-        sendTotal: item.ssendTotal
-      }))
-      }else{
+          ...item,
+          planDetailId: item.id,
+          fileList01: [],
+          fileList02: [],
+          manufactureDate: '',
+          expirationDate: "",
+          sendTotal: item.ssendTotal
+        }))
+      } else {
         this.editDetails()
       }
-      
+
     }
   },
   activated() {
-    const finallyData = this.historyCache({ packagingFm: ''}, 0)
+    const finallyData = this.historyCache({ packagingFm: '' }, 0)
     const goodsData = this.goodsData.concat(finallyData)
     let obj = {}
     this.goodsData = goodsData.reduce((cur, next) => {
@@ -189,13 +190,13 @@ export default {
     this.$store.dispatch('public/setMateriaList', this.goodsData)
   },
   methods: {
-     init() {
+    init() {
       const finallyData = this.historyCache({ packagingFm: '' }, 0)
-      
+
       this.goodsData.push(...finallyData)
-     
+
     },
-        historyCache(obj, index, isDefault) {
+    historyCache(obj, index, isDefault) {
       const data = this.$store.state.public.materiaList || []
       const historyList = this.$store.state.public.historyList || {}
       if (historyList) {
@@ -209,14 +210,14 @@ export default {
         return obj
       }
       const finallyData = data.map((item) => Object.assign({}, item, {
-        packagingFm:item.packagingFm
+        packagingFm: item.packagingFm
       }))
       return finallyData
     },
     fieldClick($event, name, index) {
       this.$refs.historyList.init($event, name, index)
     },
-     historyClick(data, name, index, historyIndex) {
+    historyClick(data, name, index, historyIndex) {
       let obj = { packagingFm: '' }
       let finallyData = this.historyCache(obj, historyIndex, true)
       for (var key in finallyData) {
@@ -257,24 +258,24 @@ export default {
             })
           }
 
-           let  params = {
-              shippingDate: this.params.shippingDate,
-              arrivalDate: this.params.arrivalDate,
-              carNumber: this.params.carNumber,
-              contacts: this.params.contacts,
-              contactsPhone: this.params.contactsPhone,
-              contractName: this.params.contractName,
-              fileByList: this.params.fileByList,
-              fileList: this.params.fileList,
-              id: this.params.id,
-              oddNumbers: this.params.oddNumbers,
-              planId: this.params.planId,
-              planName: this.params.planName,
-              sectionName: this.params.sectionName,
-              shipmentBatchNumber: this.params.shipmentBatchNumber,
-              shippingAddress: this.params.shippingAddress,
-            }
-             // 存到缓存里
+          let params = {
+            shippingDate: this.params.shippingDate,
+            arrivalDate: this.params.arrivalDate,
+            carNumber: this.params.carNumber,
+            contacts: this.params.contacts,
+            contactsPhone: this.params.contactsPhone,
+            contractName: this.params.contractName,
+            fileByList: this.params.fileByList,
+            fileList: this.params.fileList,
+            id: this.params.id,
+            oddNumbers: this.params.oddNumbers,
+            planId: this.params.planId,
+            planName: this.params.planName,
+            sectionName: this.params.sectionName,
+            shipmentBatchNumber: this.params.shipmentBatchNumber,
+            shippingAddress: this.params.shippingAddress,
+          }
+          // 存到缓存里
           this.$store.dispatch('public/setEditGoods', params)
         }
       })
@@ -346,31 +347,31 @@ export default {
     },
     editBack(text, id) {
       // 如果缓存里有数据就带变有可选地物资
-     this.$router.push({ path: '/selectGoods', query: { text: text, id: id,id:this.goodsId,planId:this.planId } })
+      this.$router.push({ path: '/selectGoods', query: { text: text, id: id, id: this.goodsId, planId: this.planId } })
 
     },
     delgoods(index) {
-      this.goodsData.splice(index,1)
+      this.goodsData.splice(index, 1)
     },
     validateForm() {
       // 存储所有错误信息
       const errors = {};
-    
+
       // 遍历 goodsData，逐项验证
       this.goodsData.forEach((item, index) => {
-         // 发货数量：必填且必须大于0
+        // 发货数量：必填且必须大于0
         if (item.sendTotal === undefined || item.sendTotal === null || item.sendTotal === "") {
           errors[`goodsData[${index}].sendTotal`] = "请填写发货数量";
         } else if (Number(item.sendTotal) <= 0) {
           errors[`goodsData[${index}].sendTotal`] = "发货数量必须大于0";
         }
-         if (!item.packagingFm) {
+        if (!item.packagingFm) {
           errors[`goodsData[${index}].packagingFm`] = "请填写包装形式";
         }
-         if (!item.manufactureDate) {
+        if (!item.manufactureDate) {
           errors[`goodsData[${index}].manufactureDate`] = "请选择生产日期";
         }
-         if (!item.field2) {
+        if (!item.field2) {
           errors[`goodsData[${index}].field2`] = "请填写收货地址";
         }
         if (!item.supplyDate) {
@@ -386,22 +387,22 @@ export default {
           errors[`goodsData[${index}].fileList02`] = "请上传厂检报告附件";
         }
       });
- 
-        // 如果有错误，显示第一个错误并返回 false
-        if (Object.keys(errors).length > 0) {
-          const firstErrorKey = Object.keys(errors)[0];
-          Toast.fail(errors[firstErrorKey]); // 显示第一个错误提示
-          return false;
-        }
-      
-        return true; // 验证通过
-      },
+
+      // 如果有错误，显示第一个错误并返回 false
+      if (Object.keys(errors).length > 0) {
+        const firstErrorKey = Object.keys(errors)[0];
+        Toast.fail(errors[firstErrorKey]); // 显示第一个错误提示
+        return false;
+      }
+
+      return true; // 验证通过
+    },
     save() {
       if (this.goodsData.length == 0) {
         Toast('没有可提交的内容');
         return;
       }
-      if(!this.validateForm()){
+      if (!this.validateForm()) {
         return
       }
       // 给字段加缓存
@@ -415,7 +416,7 @@ export default {
           this.$set(fileByList, "hgz", item.fileList01);
         }
         if (item.fileList02.length > 0) {
-          this.$set(fileByList, "cjbg", item.fileList01);
+          this.$set(fileByList, "cjbg", item.fileList02);
         }
         item.fileByList = JSON.stringify(fileByList);
       });
@@ -429,7 +430,7 @@ export default {
         }
         demandSaveSendGoods(params).then((res) => {
           if (res.code == 0) {
-             this.$store.dispatch('public/setHistoryList', obj)
+            this.$store.dispatch('public/setHistoryList', obj)
             Toast.success(res.data);
             this.$router.push({ path: "/Information" })
           }
@@ -460,7 +461,7 @@ export default {
       }
     },
     formattedCreateDate(timestamp) {
-      if(!timestamp) return ''
+      if (!timestamp) return ''
       const date = new Date(timestamp);
       const year = date.getFullYear();
       const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 月份加0
@@ -523,10 +524,12 @@ export default {
   }
 
 }
+
 .detail-list-ul {
-    padding: 0.125rem 0.3rem 0.125rem 0.525rem;
-    // margin: 0 !important;
+  padding: 0.125rem 0.3rem 0.125rem 0.525rem;
+  // margin: 0 !important;
 }
+
 .list_title {
   position: relative;
 }
