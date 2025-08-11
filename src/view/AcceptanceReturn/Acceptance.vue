b7c514e38e4939095c914511aff9993704219aa4
liuao <2280558330@qq.com>
2025-08-11 19:07:47 +0800

    跳转回来后刷新列表

--- src/view/AcceptanceReturn/Acceptance.vue
+++ src/view/AcceptanceReturn/Acceptance.vue
@@ -188,6 +188,7 @@ export default {
     }
   },
   activated() {
+    this.allRefresh();
     this.scrollPositionInit(this.className, this.allFinished)
     if (this.$route.params.refresh) {
       this.waitRefresh()
