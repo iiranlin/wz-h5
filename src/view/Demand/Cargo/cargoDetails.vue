be9ab44358b64374aace4118c63e83d28275ea01
liuao <2280558330@qq.com>
2025-08-21 16:45:39 +0800

    去掉吸顶

--- src/view/Demand/Cargo/cargoDetails.vue
+++ src/view/Demand/Cargo/cargoDetails.vue
@@ -40,7 +40,7 @@
     </div>
 
     <div class="tabs">
-      <van-tabs sticky v-model="menuActiveIndex" color="#0571ff" title-active-color="#0571ff"
+      <van-tabs v-model="menuActiveIndex" color="#0571ff" title-active-color="#0571ff"
                 title-inactive-color="#2e2e2e">
         <van-tab title="发货基本信息" name="发货基本信息">
           <van-list>
