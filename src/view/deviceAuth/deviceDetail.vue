db7c5ea1037cd1752516748a6e55e74207f8730b
ranlin <mumu@irlin.cn>
2025-09-05 15:38:30 +0800

    fix 授权选项调整

--- src/view/deviceAuth/deviceDetail.vue
+++ src/view/deviceAuth/deviceDetail.vue
@@ -135,7 +135,7 @@ export default {
       },
       showUnitTypePicker: false,
       showDeviceTypePicker: false,
-      unitTypeOptions: ['上海局集团公司', '枢纽指挥部','施工单位','监理单位','物代公司','供应商'],
+      unitTypeOptions: [ '施工单位','监理单位','供应商'],
       deviceTypeOptions: ['手机', '平板']
     };
   },
