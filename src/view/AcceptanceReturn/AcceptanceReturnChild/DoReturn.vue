35e4f7ce0c05a7de3d7f22ff7528e080ddbf8f81
ranlin <mumu@irlin.cn>
2025-09-05 11:28:10 +0800

    fix 操作人调整

--- src/view/AcceptanceReturn/AcceptanceReturnChild/DoReturn.vue
+++ src/view/AcceptanceReturn/AcceptanceReturnChild/DoReturn.vue
@@ -51,7 +51,7 @@
                 dataList.materialCirculationDetailsTableDTOS.length : 0 }}</span> 项</span>
           </li>
           <li>
-            <span>操作人：<span class="li-span-click">{{dataList.createUserName}}</span></span>
+            <span>操作人：<span class="li-span-click">{{dataList.storeOperator}}</span></span>
 
           </li>
         </ul>
