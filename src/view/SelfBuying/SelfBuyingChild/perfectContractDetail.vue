e853efca00c304c15806d4cca879c7e21427ecdb
ranlin <mumu@irlin.cn>
2025-11-12 16:30:58 +0800

    fix: 所有审核都支持下级审核人多选

--- src/view/SelfBuying/SelfBuyingChild/perfectContractDetail.vue
+++ src/view/SelfBuying/SelfBuyingChild/perfectContractDetail.vue
@@ -230,7 +230,7 @@ import { mergeByActId } from '@/utils/index.js'
 import { wfHistoryList } from '@/api/myToDoList'
 import indexMixin from '@/view/mixins'
 
-import { auditReject, wfNextAssignee, auditApprove } from '@/api/myToDoList'
+import { auditReject, wfNextAssigneeMore, auditApprove } from '@/api/myToDoList'
 import eventBus from '@/utils/eventBus.js'
 
 import { materialPurchaseContractdetail } from '@/api/prodmgr-inv/SelfBuying'
@@ -275,6 +275,7 @@ export default {
 
   data() {
     return {
+      multiple: false,
       rQueryId: '',
       activeNames: [0],
       from: "",
@@ -386,13 +387,15 @@ export default {
         name: "ApproverChoice",
         params: {
           obj: JSON.stringify(this.assigneeList),
+          selectd: JSON.stringify(this.candidateUser) ,
+          multiple: this.multiple
         },
       });
     },
     //选择审核人回调
     approverChoiceCallBack(item) {
-      this.assigner = item.nickName;
-      this.candidateUser.push(item.id);
+       this.assigner = item.map(x=> x.nickName).join('，');
+       this.candidateUser = item.map(x => x.id);
     },
     //选择审核人取消
     handleAssigneeCancel() {
@@ -411,11 +414,14 @@ export default {
         message: "正在加载...",
         forbidClick: true
       });
-      wfNextAssignee(this.listObj.taskId).then(({ data }) => {
-        if (data && data.length > 0) {
-          this.assigneeList = data;
-          this.assigneePopupShow = true;
-        } else {
+      wfNextAssigneeMore(this.listObj.taskId).then(({ data }) => {
+        if(data.data && data.data.length > 0){
+                this.assigneeList = data.data;
+                 if(data.nextTaskType ==="APPOINT_AND"){
+                    this.multiple = true;
+                  }
+                this.assigneePopupShow = true;
+            } else {
           this.approvalRequest();
         }
       }).catch((error) => {
